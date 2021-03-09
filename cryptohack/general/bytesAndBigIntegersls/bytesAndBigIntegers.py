8d8c0d4a4fada856219528e087ec572e24dc60e9
ak3rmit <adrianbeckdev@gmail.com>
2021-03-09 08:36:40 +0100

    Solved bytesAndBigIntegers on cryptohack

---
+++ cryptohack/general/bytesAndBigIntegersls/bytesAndBigIntegers.py
@@ -0,0 +1,12 @@
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import long_to_bytes
+
+
+mystery = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+
+ans = long_to_bytes(mystery)
+
+print(ans)
+
+
