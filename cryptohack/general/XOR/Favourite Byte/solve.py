03afc64811c0af6c3ad0994f6a1c118a353912f0
ak3rmit <adrianbeckdev@gmail.com>
2021-03-17 14:25:26 +0100

    Solving Math Challenges

--- cryptohack/general/XOR/Favourite Byte/solve.py
+++ cryptohack/general/XOR/Favourite Byte/solve.py
@@ -3,7 +3,7 @@ import binascii
 # Given encoded xor string with a single byte
 bytes_array = ['A', 'B', 'C','D','0','1']
 
-favourite_string = binascii.unhexlify("73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d")
+favourite_string = binascii.unhexlify("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
 print("Decoded Hex: " + str(favourite_string))
 
 # xoring with all 1 byte == means it's from
@@ -19,8 +19,8 @@ for byt in bytes_array:
 byte= 0x00
 
 for i in range(256):
-    flag=xor(favourite_string, byte).decode("utf-8")
-    if("crypto" in flag):
-        print(flag)
+    flag1=xor(favourite_string, byte).decode("utf-8")
+    if("crypto" in flag1):
+        print(flag1)
         break;
     byte=byte + 0x01
